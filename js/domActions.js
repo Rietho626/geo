6899a05761dfc846fa98246ed8bb21fb9cdcaacc
Rietho626 <rietho626@gmail.com>
2026-03-24 16:30:06 +0100

    Testing dom action import

--- js/domActions.js
+++ js/domActions.js
@@ -1,1 +1,12 @@
-{\rtf1}
+export default function getDomActions(word){
+    return new DomActions(word);
+}
+
+class DomActions{
+    constructor(test){
+        this.test = test;
+    }
+    bark(){
+        console.log(this.test + "woof");
+    }
+}
