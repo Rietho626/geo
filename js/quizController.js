1095d57a64b7f14772093dad35c649388a0dd03a
Rietho626 <rietho626@gmail.com>
2026-04-24 11:15:10 +0200

    hide topics changed to toggle

--- js/quizController.js
+++ js/quizController.js
@@ -32,7 +32,7 @@ settings.addEventListener("click", (e)=>{
 
 expandTopics.addEventListener("click", (e)=>{
     console.log(document.documentElement.clientWidth);
-    navUl.style.transform = "translateX(0px)";
+    navUl.classList.toggle("covered");
 })
 
 const themes = new Map([
