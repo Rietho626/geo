ee0763341b7120de4b3ca3607641338cea0994f6
Rietho626 <rietho626@gmail.com>
2026-04-28 14:29:39 +0200

    Update quizController.js

--- js/quizController.js
+++ js/quizController.js
@@ -32,9 +32,8 @@ settings.addEventListener("click", (e)=>{
 
 expandTopics.addEventListener("click", (e)=>{
     console.log(document.documentElement.clientWidth);
-    navUl.style.transition = "0.3s";
     navUl.classList.toggle("covered");
-    navUl.style.transition = "0s";
+
 })
 
 const themes = new Map([
